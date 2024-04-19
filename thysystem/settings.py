1aa7240372c0d5771cc64b3ce02ddbffd1c90145
eliezerjoel <kagulirejoeleliezer@gmail.com>
2024-04-19 20:49:05 +0300

    vercel

--- thysystem/settings.py
+++ thysystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)msm6f^)@m*@0q$+tne(ml2_!7f$!z_06e!rul)ot=)fqgt64z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
